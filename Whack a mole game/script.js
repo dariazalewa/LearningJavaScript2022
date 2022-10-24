8e67b3a5c04306ac6e300b918fe52e7c58a02050
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-10-24 13:51:39 +0200

    We use const

---
+++ Whack a mole game/script.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time')
+const score = document.querySelector('#score')
