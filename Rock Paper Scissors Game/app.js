3798d3d2a457a5c544470273a550395b70094c4f
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-10-24 12:27:56 +0200

    The game is done

--- Rock Paper Scissors Game/app.js
+++ Rock Paper Scissors Game/app.js
@@ -4,24 +4,50 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
     
     if (randomNumber === 1){
-        computerChoice = 'rock'
+        computerChoice = 'Rock'
     }
     if (randomNumber === 2){
-        computerChoice = 'scissors'
+        computerChoice = 'Scissors'
     }
     if (randomNumber === 3){
-        computerChoice = 'paper'
+        computerChoice = 'Paper'
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'Its a draw!'
+    }
+    if (computerChoice === 'Rock' && userChoice === "Paper") {
+        result = 'You win!'
+    }
+    if (computerChoice === 'Rock' && userChoice === "Scissors") {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'Paper' && userChoice === "Rock") {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'Paper' && userChoice === "Scissors") {
+        result = 'You win!'
+    }
+    if (computerChoice === 'Scissors' && userChoice === "Paper") {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'Scissors' && userChoice === "Rock") {
+        result = 'You win!'
+    }
+    resultDisplay.innerHTML = result
 }
