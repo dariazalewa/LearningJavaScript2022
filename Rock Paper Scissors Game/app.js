c9a8b04390d5c33a19686430ade810e6646ef151
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-10-24 11:20:10 +0200

    getElementById

--- Rock Paper Scissors Game/app.js
+++ Rock Paper Scissors Game/app.js
@@ -1,1 +1,4 @@
 const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
