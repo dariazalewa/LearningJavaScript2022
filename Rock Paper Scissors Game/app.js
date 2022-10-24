767d0e19049616e94f5a8d7c5cc03e5a949b9a0d
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-10-24 11:28:45 +0200

    The user can select the required option with the button

--- Rock Paper Scissors Game/app.js
+++ Rock Paper Scissors Game/app.js
@@ -1,4 +1,11 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
