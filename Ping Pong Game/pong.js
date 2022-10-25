02bbd7d6c66ee29d5efcf4c08e2b5deb96e00151
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-10-25 11:26:01 +0200

    Made a ball object.

--- Ping Pong Game/pong.js
+++ Ping Pong Game/pong.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 
-context.fillStyle="black"
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    velocityX : 5,
+    velocityY : 5,
+    speed : 7,
+    color : "WHITE"
+}
