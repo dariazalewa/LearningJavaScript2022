70c3eab8909f630332df0ccb420ca7f0fdf6029f
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-11-07 10:21:35 +0100

    The file app.js is ready.

--- Flight widget inspired by kubowania tutorial/app.js
+++ Flight widget inspired by kubowania tutorial/app.js
@@ -37,3 +37,80 @@ let flights = [
     remarks: "DELAYED"
   }
 ]
+const destinations = ["BANGKOK", "PRAGA", "SOFIA", "WIEDEŃ", "FRANKFURT", "RZYM"]
+const remarks = ["ON TIME", "DELAYED", "CANCELLED"]
+let hour = 15
+
+function populateTable() {
+    for (const flight of flights) {
+      const tableRow = document.createElement("tr")
+      const tableIcon = document.createElement("td")
+      tableIcon.textContent = "✈"
+      tableRow.append(tableIcon)
+  
+      for (const flightDetail in flight) {
+        const tableCell = document.createElement("td")
+        const word = Array.from(flight[flightDetail])
+  
+        for (const [index, letter] of word.entries()) {
+          const letterElement = document.createElement("div")
+          setTimeout(() => {
+            letterElement.classList.add('flip')
+            letterElement.textContent = letter
+            tableCell.append(letterElement)
+          }, 100 * index)
+  
+  
+        }
+        tableRow.append(tableCell)
+      }
+      tableBody.append(tableRow)
+    }
+  }
+  populateTable() 
+  
+  function generateRandomLetter() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+  }
+  
+  function generateRandomNumber(maxNumber) {
+    const numbers = "0123456789"
+    if (maxNumber) {
+      const newNumbers = numbers.slice(0, maxNumber)
+      return newNumbers.charAt(Math.floor(Math.random() * newNumbers.length))
+    } else {
+      return numbers.charAt(Math.floor(Math.random() * numbers.length))
+    }
+  }
+  
+  function generateTime() {
+    let displayHour = hour
+    if (hour < 24) {
+      hour++
+    }
+    if (hour >= 24) {
+      hour = 1
+      displayHour = hour
+    }
+    if (hour < 10) {
+      displayHour = "0" + hour
+    }
+    return displayHour +  ":" + generateRandomNumber(5) + generateRandomNumber()
+  }
+  
+  function shuffleUp() {
+    flights.shift()
+    flights.push({
+      time: generateTime(),
+      destination: destinations[Math.floor(Math.random() * destinations.length)],
+      flight: generateRandomLetter() + generateRandomLetter() + " " + generateRandomNumber() + generateRandomNumber() + generateRandomNumber(),
+      gate: generateRandomLetter() + " " + generateRandomLetter() + generateRandomLetter(),
+      remarks: remarks[Math.floor(Math.random() * remarks.length)]
+    })
+    tableBody.textContent = ""
+    populateTable()
+  }
+  
+  
+  setInterval(shuffleUp, 5000)
