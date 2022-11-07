f54f44035be43033e75d672fa5ec79379a796937
dariazalewa <94965342+dariazalewa@users.noreply.github.com>
2022-11-07 10:04:49 +0100

    Flights are ready.

---
+++ Flight widget inspired by kubowania tutorial/app.js
@@ -0,0 +1,39 @@
+const tableBody = document.getElementById('table-body')
+
+let flights = [
+  {
+    time: "10:00",
+    destination: "FRANKFURT",
+    flight: "OX 203",
+    gate: "31",
+    remarks: "ON TIME"
+   },
+  {
+    time: "11:10",
+    destination: "WIEDEŃ",
+    flight: "OS 626",
+    gate: "28",
+    remarks: "BOARDING"
+   },
+   {
+    time: "13:30",
+    destination: "SOFIA",
+    flight: "LO 631",
+    gate: "23N",
+    remarks: "CANCELLED"
+  },
+  {
+    time: "14:01",
+    destination: "PRAGA",
+    flight: "LO 523",
+    gate: "6",
+    remarks: "ON TIME"
+  },
+  {
+    time: "15:22",
+    destination: "BANGKOK",
+    flight: "LO 783",
+    gate: "36",
+    remarks: "DELAYED"
+  }
+]
